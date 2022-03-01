cd3e0f5f29f9d8176659af45a8b05bc6b307a4b1
newtonchakma <newtonchakma304@gmail.com>
2022-03-01 12:11:39 +0600

    setting show phone

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const getAllPhone =phone =>{
  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
  fetch(url)
  .then(response => response.json())
- .then(data => displayAllPhone(data.data))
+ .then(data => displayAllPhone(data.data.slice(0,20)))
 
  document.getElementById('search-box').value = '';
  console.log(searchValue);
@@ -12,25 +12,34 @@ const getAllPhone =phone =>{
 
 const displayAllPhone = phones =>{
 
+
 for(const phone of phones){
+
+    
     const parent = document.getElementById('show-phone');
-    parent.innerHTML =`
-    <div class="col">
-    <div class="card h-50">
-      <img src="${phone.image}" class="card-img-top p-4" alt="..." >
-      <div class="card-body">
-        <h5 class="card-title">Phone name : ${phone.phone_name}</h5>
-        <h6 class="card-title"> Brand : ${phone.brand}</h6>
-        
-      </div>
+
+    const div = document.createElement('div');
+    div.innerHTML = `  
+      <div class="card shadow-lg mt-4 round">
+    <img src="${phone.image}" class="card-img-top p-2" alt="">
+    <div class="card-body">
+      <h5 class="card-title">Brand : ${phone.brand}</h5>
+      <h6 class="card-title">Phone name : ${phone.phone_name}</h6>
+      
       <div class="card-footer mx-auto">
-        <button class=" btn btn-primary">Details</button>
-      </div>
+      <button class="btn btn-primary w-100" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Details</button>
+   
     </div>
-  </div>
-    
-    
-    `
-    console.log(phone);
+      
+     
+    </div>
+    </div>`
+    parent.appendChild(div)
+  
+
+
 }
-}
+} 
+
+
+    
