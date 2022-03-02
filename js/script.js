57f5e5abfb08894b467a5a39864aa3119cdd7c02
newtonchakma <newtonchakma304@gmail.com>
2022-03-02 16:05:56 +0600

    error dandle

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
 
 // get all phone 
-const getAllPhone =phone =>{
+const getAllPhone = phone =>{
  const searchValue = document.getElementById('search-box').value;
-// get phone api link 
- const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
- if(searchValue === '' || searchValue != url){
-   document.getElementById('error').style.display = 'block';
- }
  
+console.log("phone ", phone);
+ if(searchValue === ''){
+   document.getElementById('error').style.display = 'block';
+  document.getElementById('error2').style.display = 'none';
+ }
+ else if(phone === undefined){
+  document.getElementById('error2').style.display = 'block';
+  document.getElementById('error').style.display = 'none';
+ }
+  // get phone api link 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
   fetch(url)
   .then(response => response.json())
   .then(data => displayAllPhone(data.data.slice(0,20)))
  
- 
+
+ searchValue.innerHTML = '';
+  document.getElementById('show-details').innerHTML = '';
  
 // clear input field
  document.getElementById('search-box').value = '';
- console.log(searchValue);
+ 
 }
 
 // display all phone 
 const displayAllPhone = phones =>{
+  const parent = document.getElementById('show-phone');
+  parent.innerHTML ='';
 for(const phone of phones){
- 
-    const parent = document.getElementById('show-phone');
     const div = document.createElement('div');
-    document.getElementById('error').style.display = 'none';
+ console.log(phone);
+     document.getElementById('error').style.display = 'none';
+     document.getElementById('error2').style.display = 'none';
     div.innerHTML = `  
       <div class="card shadow-lg mt-4 rounded">
     <img src="${phone.image}" class="img-fluid card-img-top p-2" alt="">
     <div class="card-body">
-      <h5 class="card-title">Brand : ${phone.brand}</h5>
-      <h6 class="card-title">Phone name : ${phone.phone_name}</h6>
+      <h5 class="card-title"> <strong>Brand : </strong> ${phone.brand}</h5>
+      <h6 class="card-title"> <strong>Phone name  : </strong> ${phone.phone_name}</h6>
       
       <div class="footer mx-auto">
-      <button onclick="getPhoneDetails('${phone.slug}')" class="btn btn-primary w-100" type="button">Details</button>
+   <a href="#header"><button onclick="getPhoneDetails('${phone.slug}')" class="btn btn-primary w-100" type="button">Details</button></>
    
     </div>
       
@@ -60,6 +70,7 @@ fetch(url)
 // show phone details
 const displayPhoneDetails = info =>{
     const showDetails = document.getElementById('show-details');
+    
     // set default values
    if(info.others===undefined || info.releaseDate.length <= 0){
      info.others={WLAN:'No',NFC:'No',GPS:'No',Radio:'No',USB:'No',Bluetooth:'No'};
@@ -96,3 +107,7 @@ const displayPhoneDetails = info =>{
   console.log(info);
 }
 
+/*  function scrollTop(){
+
+  Window.scrollTo(0,0)
+}  */
